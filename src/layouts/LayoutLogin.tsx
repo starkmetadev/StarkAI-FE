1b16cd7928e5875355f4fec997f851a68b2ea2df
Shadow001017 <ryanniit0x902@gmail.com>
2024-02-05 09:03:27 -0500

    update

--- src/layouts/LayoutLogin.tsx
+++ src/layouts/LayoutLogin.tsx
@@ -1,73 +1,18 @@
-import { useGoogleLogin, GoogleLogin } from "@react-oauth/google";
-import { GoogleLogin as GoogleAuth } from "react-google-login";
-import { jwtDecode } from "jwt-decode";
+import { GoogleLogin } from "google-login-react";
 import { useUser } from "../context/UserContext";
 import { Navigate, Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import axios from "axios";
-import useWindowWidth from "../hooks/useWindowSize";
-import { useEffect, useState } from "react";
 
 const Login = () => {
-  const windowSize = useWindowWidth();
-  const [buttonWidth, setButtonWidth] = useState(0);
-
-  const smBreakpoint = 768;
-
-  useEffect(() => {
-    let wSize = windowSize < smBreakpoint ? windowSize : 204;
-    setButtonWidth(wSize);
-  }, [windowSize]);
-
-  console.log("Window Size:", windowSize);
-  // const googleLogin = u  seGoogleLogin({
-  //   onSuccess: async (credentialResponse: any) => {
-  //     if (!credentialResponse.credential) return;
-  //     let googleData: { name: string; email: string } = jwtDecode(
-  //       credentialResponse.credential
-  //     );
-  //     const data = {
-  //       username: googleData.name,
-  //       email: googleData.email,
-  //     };
-  //     try {
-  //       const res = await axios.post(
-  //         `${process.env.REACT_APP_BACKEND_API}/login`,
-  //         data
-  //       );
-  //       if (res.data.message === "Success") {
-  //         console.log("Success");
-  //         setUser(JSON.stringify(data));
-  //       } else {
-  //         console.log("Fail");
-  //       }
-  //     } catch (error) {
-  //       console.error("Login Failed", error);
-  //     }
-  //   },
-  //   onError: () => {
-  //     console.log("Login Failed");
-  //   },
-  // });
-
-  const { user, setUser }: any = useUser();
-  if (user && user !== "none") {
-    return <Navigate to="/app" />;
-  }
-
-  // const handleLoginClick = () => {
-  //   googleLogin();
-  // };
-
   const onLogin = async (credentialResponse: any) => {
-    if (!credentialResponse.credential) return;
-    let googleData: { name: string; email: string } = jwtDecode(
-      credentialResponse.credential
-    );
+    if (!credentialResponse) return;
+
     const data = {
-      username: googleData.name,
-      email: googleData.email,
+      username: credentialResponse.name,
+      email: credentialResponse.email,
     };
+
     const res = await axios.post(
       `${process.env.REACT_APP_BACKEND_API}/login`,
       data
@@ -80,32 +25,48 @@ const Login = () => {
     setUser(JSON.stringify(data));
   };
 
+  const { user, setUser }: any = useUser();
+  if (user && user !== "none") {
+    return <Navigate to="/app" />;
+  }
+
   return (
     <div className="p-10 w-full h-screen bg-black font-chakra">
       <div className="w-full h-full flex flex-row justify-between items-center">
-        <div className="w-[380px] h-full px-16 py-10 bg-darkPrimary rounded-tl-lg rounded-bl-lg sm:w-full sm:rounded-tr-lg sm:rounded-br-lg sm:px-8">
+        <div className="w-[380px] h-full px-16 py-10 bg-[#171717] rounded-tl-lg rounded-bl-lg sm:w-full sm:rounded-tr-lg sm:rounded-br-lg sm:px-8">
           <div className="flex flex-col justify-between items-center gap-12 w-full">
             <div className="w-full flex flex-col gap-2 items-center">
               <img src="./favicon.ico" className="w-[64px]" alt="logo" />
-              <div className="text-md font-bold">Sign up or Login with</div>
-              {/* <button onClick={handleLoginClick}>Login with Google</button> */}
-              <div className="w-full">
+              <div className="text-md font-bold mb-5">
+                Sign up or Login with
+              </div>
+              <div
+                className="w-full flex items-center justify-center"
+                id="google_login"
+              >
                 <GoogleLogin
-                  onSuccess={(credentialResponse) =>
-                    onLogin(credentialResponse)
-                  }
-                  onError={() => {
-                    console.log("Login Failed");
-                  }}
-                  width={`${buttonWidth}px`}
-                />
+                  clientId={process.env.REACT_APP_CLIENT_ID!}
+                  onSuccess={(res) => onLogin(res)}
+                  onError={(err) => console.log(err)}
+                >
+                  <button className="login-button flex flex-row">
+                    <Icon
+                      icon="logos:google-icon"
+                      className="h-[20px] w-auto"
+                    />
+                    <span className="">Google</span>
+                  </button>
+                </GoogleLogin>
               </div>
               <Link
                 to="https://discord.com/invite/starkmeta"
-                className="bg-white flex px-3 py-2 rounded-[4px] items-center cursor-pointer w-full gap-2"
+                className="login-button"
               >
-                <Icon icon="ic:round-discord" className="text-black w-6 h-6" />
-                <span className="flex w-full place-content-left text-black text-[14px] font-medium">
+                <Icon
+                  icon="logos:discord-icon"
+                  className="text-white w-6 h-6"
+                />
+                <span className="flex w-full place-content-left text-white text-[14px] font-medium">
                   Discord
                 </span>
               </Link>
